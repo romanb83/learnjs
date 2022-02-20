6e1fd8ae4bee81663f3d7cb3ab77842a4e15d8d1
romanb83 <roman.bikov@gmail.com>
2022-02-20 20:23:33 +0300

    Урок 1.1

--- js/script.js
+++ js/script.js
@@ -11,13 +11,11 @@ let appData = {
     savings: false,
 }
 
-let a1 = prompt('Введите обязательную статью расходов в этом месяце',''),
-    a2 = prompt('Во сколько обойдется?',''),
-    a3 = prompt('Введите обязательную статью расходов в этом месяце',''),
-    a4 = prompt('Во сколько обойдется?','');
-
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце',''),
+        b = prompt('Во сколько обойдется?','');
+    appData.expenses[a] = b;
+}
 
 alert(appData.budget/30);
 
